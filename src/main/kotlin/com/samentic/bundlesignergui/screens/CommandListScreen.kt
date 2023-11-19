4a775fa348abc6f2d51f577c301f169e5d75244a
Mohsen Sameti <s.mn.sameti@gmail.com>
2023-11-19 21:55:14 +0330

    add CommandListScreen

--- src/main/kotlin/com/samentic/bundlesignergui/screens/CommandListScreen.kt
+++ src/main/kotlin/com/samentic/bundlesignergui/screens/CommandListScreen.kt
@@ -3,39 +3,33 @@ package com.samentic.bundlesignergui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 
-class StartupScreen : Screen {
+class CommandListScreen : Screen {
 
     @Composable
     override fun Content() {
-
-        LaunchedEffect(Unit) {
-            println("getting java version...")
-            runCatching {
-                val process = ProcessBuilder()
-                    .command("java", "-version")
-                    .redirectErrorStream(true)
-                    .start()
-                process.inputStream.bufferedReader().readText()
-            }.onFailure {
-                it.printStackTrace()
-            }.onSuccess {
-                println(it)
+        val navigateToGenBin = remember {
+            {
+                // TODO
             }
         }
+
 
         Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxSize()
         ) {
-            Text("Checking Java Version ...")
+            Button(onClick = navigateToGenBin) {
+                Text("Generate Signed Digest")
+            }
         }
     }
 }
